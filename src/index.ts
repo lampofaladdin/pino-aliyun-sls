44bfdf40ef8dc7c69de534fa82d7a9b838b11750
aladdin <wangdingding@42how.com>
2023-11-03 19:03:38 +0800

    fix: 修改bug

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,16 @@ export default async function (opts: PinoAliSLSOption) {
         endpoint,
         projectName,
         logStoreName,
-        logKeys
+        logKeys,
     } = opts;
 
-    if (!accessKeyId || !secretAccessKey || !endpoint || !projectName || !logStoreName) {
+    if (
+        !accessKeyId ||
+    !secretAccessKey ||
+    !endpoint ||
+    !projectName ||
+    !logStoreName
+    ) {
         throw new Error(
             'The required options(accessKeyId, secretAccessKey, endpoint, projectName, logStoreName) are missing'
         );
@@ -24,13 +30,16 @@ export default async function (opts: PinoAliSLSOption) {
         endpoint,
         apiVersion: '2015-06-01', //SDK版本号，固定值。
     });
+    
     return build(async (source) => {
-        for await (const obj of source) {
+        for await (const logRecord of source) {
 
-            const logContents = Object.keys(obj).map((item) => {
+            // 转换日志对象
+            // TODO 无法序列化多层级的对象
+            const logContents = Object.keys(logRecord).map((item) => {
                 return {
                     key: item,
-                    value: `${obj[item]}`,
+                    value: `${logRecord[item]}`,
                 };
             });
 
@@ -38,15 +47,14 @@ export default async function (opts: PinoAliSLSOption) {
                 projectName,
                 logStoreName,
                 logGroup: {
-                    // 必选，写入的日志数据。
                     logs: [
                         {
-                            time: Math.floor((logKeys?.time ? (new Date(logKeys.time) || new Date()) : new Date()).getTime() / 1000),
+                            time: getLogDate(logRecord, logKeys?.time),
                             contents: logContents,
                         },
                     ],
-                    topic: logKeys?.topic ? obj[logKeys.topic] || null : null,
-                    source: logKeys?.source ? obj[logKeys.source] || null : null,
+                    topic: logKeys?.topic ? logRecord[logKeys.topic] || null : null,
+                    source: logKeys?.source ? logRecord[logKeys.source] || null : null,
                 },
             };
             const puLogs = promisify(sls.putLogs);
@@ -54,15 +62,35 @@ export default async function (opts: PinoAliSLSOption) {
                 await puLogs.bind(sls)(param);
             } catch (e: unknown) {
                 if (e instanceof Error) {
-                    console.error(`push log to aliyun error ${e.stack}`);
-                    return; 
+                    console.error(`Push log to aliyun error ${e.stack}`);
+                    return;
                 }
-                console.error(`push log to aliyun error ${e}`);
+                console.error(`Push log to aliyun error ${e}`);
             }
         }
     });
-}
 
-export type {
-    PinoAliSLSOption
-};
+    /**
+   * 格式化日期
+   */
+    function getTimestampe(date: Date) {
+        return Math.floor(date.getTime() / 1000);
+    }
+
+    /**
+   * 获取日志时间
+   */
+    function getLogDate(log: Record<string, any>, key?: string | number) {
+        if (!key) {
+            return getTimestampe(new Date());
+        }
+        if (!log[key]) {
+            return getTimestampe(new Date());
+        }
+        const date = new Date(log[key]);
+        if (isNaN(date.getTime())) {
+            return getTimestampe(new Date());
+        }
+        return getTimestampe(date);
+    }
+}
